40570d7db23c2915c4bede92a10725bdf0ab22b1
DJRoche509 <davjroche@gmail.com>
2023-11-22 20:05:51 -0500

    Implement EventListener for email/contactForm

--- website/script.js
+++ website/script.js
@@ -1,12 +1,61 @@
 // Visitor Counter
-const counter = document.getElementById("visitor-count");
-async function updateCounter() {
-    let response = await fetch("https://bd3nqxp3kyfxqqxehujwnkdjym0wfsgw.lambda-url.us-east-2.on.aws/"); //("https://in3e4ogyqefn6n4cik3mwpuxb40jglxp.lambda-url.us-east-2.on.aws/") with Terraform ;
-    let data = await response.json();
-    counter.innerHTML = `${data}`;
-}
+// const counter = document.getElementById("visitor-count");
+// async function updateCounter() {
+//     let response = await fetch("https://bd3nqxp3kyfxqqxehujwnkdjym0wfsgw.lambda-url.us-east-2.on.aws/"); //("https://in3e4ogyqefn6n4cik3mwpuxb40jglxp.lambda-url.us-east-2.on.aws/") with Terraform ;
+//     let data = await response.json();
+//     counter.innerHTML = `${data}`;
+// }
 
 // Contact Form Submission
+const fullName = document.getElementById('name');
+const phone = document.getElementById('phone');
+const email = document.getElementById("email");
+const message = document.getElementById('message');
+// Add event listener on form container
+const submit = document.getElementsByClassName('contactform')[0];
+
+
+// Function to reset the contact form
+const resetForm = () => {
+    fullName.value = '';
+    phone.value = '';
+    email.value = '';
+    message.value = '';
+}
+
+// Code for retrieving form info and sending email
+function sendMessage() {    
+    // Gathering form value into one format 
+    let ebody = `        
+        <b>Name: &nbsp; ${fullName.value}
+        <br>
+        <b>Phone no: &nbsp; ${phone.value}
+        <br>
+        <b>Email: &nbsp; ${email.value}
+        <br>
+        <b>Message: <br> ${message.value}
+        <br>
+    `
+    console.log(fullName.value);
+    console.log(email.value, typeof(email.value));
+
+    // Email Code Here with SmtpJS
+    Email.send({
+        SecureToken: "81349cd6-892b-4f41-b056-a0eb87d613fd",
+        To: "davjroche@gmail.com",
+        From: "davjroche@gmail.com",
+        Subject: "Cloud Resume Message from " + fullName.value,
+        Body: ebody
+    }).then(() => {
+        // Confirm successful sent of message 
+        alert("Message sent successfully")
+        // call Reset() to reset the form after successful submission
+        resetForm();
+    }).catch( error => {
+        console.error('Error occurred while sending email:', error);
+        alert("Failed to send message. Please try again.");
+    });
+}
 
 
 
@@ -44,7 +93,12 @@ contactTab.addEventListener("click", function (event) {
     toggleTabs("contact");
 });
 
+submit.addEventListener('submit', e => {
+    e.preventDefault();
+    sendMessage();
+});
+
 // Initial state: Resume tab is active
 toggleTabs("resume");
 
-updateCounter();
+// updateCounter();
